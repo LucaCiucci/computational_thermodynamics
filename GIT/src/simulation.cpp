a9b94cffc78ad476874d46d64579a93b2bc4e76e
Luca Ciucci <43919066+LucaCiucci@users.noreply.github.com>
2018-10-11 15:05:06 +0200

    Add files via upload

---
+++ GIT/src/simulation.cpp
@@ -0,0 +1,322 @@
+﻿//#include "OBJ_Loader.h"
+#include "simulation.h"
+#include "mathFunctions.h"
+//#include <random>
+
+//-------------------------------------------------------------------------------------------------
+
+int Simulation::getVolumesNumber(void) const { return volumes.size(); }
+
+void Simulation::setGasParticleNumber(int _gasParticleNumber) { GasParticleNumber = _gasParticleNumber; }
+
+int Simulation::getGasParticleNumber(void) const { return GasParticleNumber; }
+
+void Simulation::setInitialTemperature(double _temperature) { temperature = _temperature; }
+
+double Simulation::getInitialTemperature(void) const { return temperature; }
+
+//-------------------------------------------------------------------------------------------------
+
+bool Simulation::addVolume(Volume volume)
+{
+	Plane plane;
+	Vector3 p1, p2, p3;
+	Vector3 v1, v2,V;
+	double Vm;
+	for (int i = 0; i < volume.mesh.Indices.size(); i += 3)
+	{
+		p1 = volume.mesh.Vertices[volume.mesh.Indices[i]].Position;
+		p2 = volume.mesh.Vertices[volume.mesh.Indices[i+1]].Position;
+		p3 = volume.mesh.Vertices[volume.mesh.Indices[i+2]].Position;
+		
+		v1 = p2 - p1;
+		v2 = p3 - p1;
+		V = v1 % v2;// triangle area vector
+		Vm = sqrt(V.X*V.X + V.Y*V.Y + V.Z*V.Z); // V's lenght
+
+		plane.a = V.X / Vm;// coefficients for the equation of the plane containing the triangle
+		plane.b = V.Y / Vm;// a*x + b*y + c*z + d = 0
+		plane.c = V.Z / Vm;
+		plane.d =
+			- plane.a * p1.X
+			- plane.b * p1.Y
+			- plane.c * p1.Z;
+
+		volume.mesh.plane.push_back(plane);
+	}
+	volume.box = boundingBox(volume);
+	volumes.push_back(volume);
+	return false;// TODO
+}
+
+bool Simulation::createGasParticles(void)
+{
+	bool someVolumes = false;
+	Box box;
+	bool n1 = false;
+	// find the box containing all the bounding boxes
+	for (int i = 0; i < volumes.size(); i++)
+	{
+		if (volumes[i].containsGas) {
+			if (!n1)
+			{
+					n1 = true;
+					box = volumes[i].box;
+					someVolumes = true;
+			}
+			box.min.X = fmin(box.min.X, volumes[i].box.min.X);
+			box.min.Y = fmin(box.min.Y, volumes[i].box.min.Y);
+			box.min.Z = fmin(box.min.Z, volumes[i].box.min.Z);
+
+			box.max.X = fmax(box.max.X, volumes[i].box.max.X);
+			box.max.Y = fmax(box.max.Y, volumes[i].box.max.Y);
+			box.max.Z = fmax(box.max.Z, volumes[i].box.max.Z);
+		}
+	}
+	if (!someVolumes) return false;// there are no volumes that should contain gas
+
+	GasParticle newPoint;
+	double rand;
+	// creates points in a random position inside the right volumes
+	for (int i = 0; i < GasParticleNumber; i++)
+	{
+		// tries random points until a contained point in the correct position is found
+		do {
+			newPoint.position.X = fRand(box.min.X, box.max.X);
+			newPoint.position.Y = fRand(box.min.Y, box.max.Y);
+			newPoint.position.Z = fRand(box.min.Z, box.max.Z);
+
+			newPoint.potential = potential(newPoint.position);
+		} while ((newPoint.potential >= 0) || !isInAllowedVolume(newPoint.position));// TODO qui c'è un errore!!!!!!!!!!!!! o frse è stato corretto o forse no?!?!?!?
+		// assign random speed
+		rand = fRand(- temperature * 2.0 / 3.0, temperature * 2.0 / 3.0); // energy along x direction (signed)
+		newPoint.speed.X = (double)sign(rand) * (double)sqrt (2.0 * abs(rand));
+		rand = fRand(-temperature * 2.0 / 3.0, temperature * 2.0 / 3.0);
+		newPoint.speed.Y = (double)sign(rand) * (double)sqrt(2.0 * abs(rand));
+		rand = fRand(-temperature * 2.0 / 3.0, temperature * 2.0 / 3.0);
+		newPoint.speed.Z = (double)sign(rand) * (double)sqrt(2.0 * abs(rand));
+		// add to the array of gas particles
+		gasParticles.push_back(newPoint);
+		/*
+		std::cout << "eccomi:::::::" << isInAllowedVolume({ -0.997497, 0.997918, -0.613392 }) << std::endl;////////////////////
+		std::cout << "eccolo-------" << isInAllowedVolume(newPoint.position) << std::endl;////////////////////
+		*/
+	}
+	return true;
+}
+
+
+//---------------------------------PRIVATE---------------------------------------------------------
+
+// find the bounding for the provided volume
+Box Simulation::boundingBox(Volume currVolume) const
+{
+	Box box;
+	box.max = box.min = currVolume.mesh.Vertices[0].Position;
+	for (int i = 1; i < currVolume.mesh.Vertices.size(); i++) {
+		box.min.X = fmin(box.min.X, currVolume.mesh.Vertices[i].Position.X);
+		box.min.Y = fmin(box.min.Y, currVolume.mesh.Vertices[i].Position.Y);
+		box.min.Z = fmin(box.min.Z, currVolume.mesh.Vertices[i].Position.Z);
+
+		box.max.X = fmax(box.max.X, currVolume.mesh.Vertices[i].Position.X);
+		box.max.Y = fmax(box.max.Y, currVolume.mesh.Vertices[i].Position.Y);
+		box.max.Z = fmax(box.max.Z, currVolume.mesh.Vertices[i].Position.Z);
+		
+	}
+	return box;
+}
+
+//the same but accepting the index of the volume
+Box Simulation::boundingBox(int volumeIndex) const
+{
+	Box _boundingBox = { {0, 0, 0,}, {0, 0, 0} };
+	if (volumeIndex < volumes.size())
+		_boundingBox = boundingBox(volumes[volumeIndex]);
+	else
+		std::cout << "OUT OF RANGE!" << std::endl;
+	return _boundingBox;
+}
+
+// list the indexes of the box containing the point
+std::vector<int> Simulation::volumesContainingVector(Vector3 position_vector) const
+{
+	std::vector<int> indexes;
+	for (int i = 0; i < volumes.size(); i++)
+	{
+		if (isInsideVolume(position_vector, i))
+			indexes.push_back(i);
+	}
+	return indexes;
+}
+
+//get potential at provided point
+double Simulation::potential(Vector3 position_vector) const
+{
+	std::vector<int> list;
+	double potential = 0;
+	list = volumesContainingVector(position_vector);
+	for (int i = 0; i < list.size(); i++)
+	{
+		potential += volumes[list[i]].deltaPotential;
+	}
+	return potential;
+}
+
+// is in a point where gas should be created
+bool Simulation::isInAllowedVolume(Vector3 position_vector) const
+{
+	for (int i = 0; i < volumes.size(); i++)
+		if (volumes[i].containsGas && isInsideVolume(position_vector, i))
+			return true;
+	return false;
+}
+
+// point is inside the provided volume
+bool Simulation::isInsideVolume(Vector3 point, int volumeIndex) const // TODO optimize (avoid unnecessary vars, too)
+{
+	//example: isInsideVolume({ 1.0, 16.0, 1.0 }, 0)
+
+	// find the box containing the volume
+	Volume currVolume = volumes[volumeIndex];
+	Box box = volumes[volumeIndex].box;
+	/*if (box.max == box.min) {
+		boundingBox(volumeIndex);
+		box = volumes[volumeIndex].box;
+	}*/
+	// if the point is outside the box,
+		// then it is outside the volume
+	if (point.X < box.min.X) return false;
+	if (point.Y < box.min.Y) return false;
+	if (point.Z < box.min.Z) return false;
+
+	if (point.X > box.max.X) return false;
+	if (point.Y > box.max.Y) return false;
+	if (point.Z > box.max.Z) return false;
+	// else
+	// RAY-CASTING
+	// trace a ray ( for example {∀t∈ℝ0+ : x = Xp+t; y = Yp; z = Zp})
+	/*
+	The easiest way to do this, I suppose, is to look at the system from one side.
+	check how many triangle the point is inside of, to do this we check if the point is above or below
+	the plane containing the triangle (ignore planes perpendicular to the view plane), and check if the
+	projection of the plane is inside the projection of the triangle (using area vectors)
+	*/
+	// for every triangle
+	Plane currPlane;
+	int counter = 0;
+	double delta;
+	Triangle2 triangle;
+	for (int i = 0; i < volumes[volumeIndex].mesh.plane.size(); i++)
+	{
+		currPlane = volumes[volumeIndex].mesh.plane[i];// to make coding easier
+		// this was the general idea -> // find intersection, if it is inside the triangle, increase counter (think about system with no solution or indetetrmined too! (solved) )
+		// suppose we are looking from the point fo Z
+		// if c = 0, this means the plane in perpendicular to our view plane, ne do not neeed to consider this triangle
+		if (currPlane.c != 0)
+		{
+			// else, f(x,y,z) = ax+b+cz+d=0. the point is above the plane if (delta := ( sign(c) * sign(f(xp,yp,zp)) ) ) = +1, otherwise the point is below, if 0 the point is on the plane
+			delta = sign(currPlane.c) * sign(currPlane.a * point.X + currPlane.b * point.Y + currPlane.c * point.Z + currPlane.d);
+			triangle =
+			{
+				{ volumes[volumeIndex].mesh.Vertices[volumes[volumeIndex].mesh.Indices[3 * i + 0]].Position.X,volumes[volumeIndex].mesh.Vertices[volumes[volumeIndex].mesh.Indices[3 * i + 0]].Position.Y },
+				{ volumes[volumeIndex].mesh.Vertices[volumes[volumeIndex].mesh.Indices[3 * i + 1]].Position.X,volumes[volumeIndex].mesh.Vertices[volumes[volumeIndex].mesh.Indices[3 * i + 1]].Position.Y },
+				{ volumes[volumeIndex].mesh.Vertices[volumes[volumeIndex].mesh.Indices[3 * i + 2]].Position.X,volumes[volumeIndex].mesh.Vertices[volumes[volumeIndex].mesh.Indices[3 * i + 2]].Position.Y }
+			};
+			// if is inside the triangle and delta > 0 (not equal, we consider points on the triangles as "outside" to simplify the creation of random points (but this is wrong?!)), then counter++
+			if (isInside2dTriangle(triangle, { point.X, point.Y }) && (delta > 0)) counter++;
+		}
+	}
+	// if counter % 2 = 1, point is inside volume
+	if (counter % 2 == 1)
+		return true;
+	return false;
+}
+
+// used inside the isInsideVolume() funcion
+bool Simulation::isInside2dTriangle(Triangle2 triangle, Vector2 point) const
+{
+	double triangleArea = abs( (triangle.v2 - triangle.v1) % (triangle.v3 - triangle.v1) );
+	double subArea1 = abs((point - triangle.v1) % (point - triangle.v2));
+	double subArea2 = abs((point - triangle.v2) % (point - triangle.v3));
+	double subArea3 = abs((point - triangle.v3) % (point - triangle.v1));
+
+	//if the areas are equal, then th point is inside the triangle
+	if ((subArea1 + subArea2 + subArea3 - triangleArea) <= Epsilon) // approximation may cause areas not to coincide perfectly
+		return true;
+	return false;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void Simulation::printVertex(int volumeIndex) const
+{
+	if ( volumes.size()  > volumeIndex)
+	{
+		for (int i = 0; i < volumes[volumeIndex].mesh.Vertices.size(); i++) {
+			std::cout << "P:\t" << volumes[volumeIndex].mesh.Vertices[i].Position.X << " \t" << volumes[volumeIndex].mesh.Vertices[i].Position.Y << " \t" << volumes[volumeIndex].mesh.Vertices[i].Position.Z << std::endl;
+		}/*
+		for (int i = 0; i < volumes[volumeIndex].mesh.Indices.size(); i++)
+			std::cout << volumes[volumeIndex].mesh.Indices[i] << std::endl;*///TODO delete, only for test
+	} else 
+	{
+		std::cout << "OUT OF RANGE!" << std::endl;
+	}
+	return;
+}
+
+void Simulation::printPlanes(int volumeIndex) const
+{
+	if (volumes.size() > volumeIndex)
+	{
+		std::cout << "\n\n\n";
+		for (int i = 0; i < volumes[volumeIndex].mesh.plane.size(); i++) {
+			std::cout
+				<< "Plane:\t a = " << volumes[volumeIndex].mesh.plane[i].a
+				<< "\t b = " << volumes[volumeIndex].mesh.plane[i].b
+				<< "\t c = " << volumes[volumeIndex].mesh.plane[i].c
+				<< "\t d = " << volumes[volumeIndex].mesh.plane[i].d
+				<< std::endl;
+		}/*
+		for (int i = 0; i < volumes[volumeIndex].mesh.Indices.size(); i++)
+			std::cout << volumes[volumeIndex].mesh.Indices[i] << std::endl;*///TODO delete, only for test
+		// TODO remove those
+	}
+	else
+	{
+		std::cout << "OUT OF RANGE!" << std::endl;
+	}
+	return;
+}
+
+void Simulation::printPoints(void) const
+{
+	std::cout << "\n\n\n";
+	double energySum = 0, energy;
+	for (int i = 0; i < gasParticles.size(); i++) {
+		energy = gasParticles[i].speed.X * gasParticles[i].speed.X / 2 +
+			gasParticles[i].speed.Y * gasParticles[i].speed.Y / 2 +
+			gasParticles[i].speed.Z * gasParticles[i].speed.Z / 2;
+		std::cout << "Point #" << i << "\tposition: x = "
+			<< gasParticles[i].position.X << " y = "
+			<< gasParticles[i].position.Y << " z = "
+			<< gasParticles[i].position.Z << "       \tspeed: x = "
+			<< gasParticles[i].speed.X << " y = "
+			<< gasParticles[i].speed.Y << " z = "
+			<< gasParticles[i].speed.Z << "              \tenergy: "
+			<< energy << std::endl;
+		energySum += energy;
+	}
+	std::cout << "\nAverage energy: " << energySum / gasParticles.size() << std::endl;
+}
+
+void Simulation::test(void) const
+{
+	std::cout << "\n\n" << isInsideVolume({ 1.01, 1.0, 1.0 }, 0) << std::endl;//non funzionaaaaaaaa (se capita su una linea (entro tolleranza epsilon su area non funziona))
+	std::cout << isInsideVolume({ 1.0, 6.0, 1.0 }, 0) << std::endl;
+	std::cout << isInsideVolume({ 1.0, 16.0, 1.0 }, 0) << std::endl;
+	std::cout << isInsideVolume({ 1.0, -6.0, 1.0 }, 0) << std::endl;
+	//std::cout           << isInsideVolume({ 0.01, 2.411, 0.012 }, 2) << std::endl;
+	std::cout << isInAllowedVolume({ -0.997497, 0.997918, -0.613392 }) << std::endl;
+	std::cout << isInsideVolume({ 1.9, 1.92, 1.93 }, 0) << std::endl;
+	std::cout << "box x:: " << volumes[0].box.min.X << "box x:: " << volumes[0].box.min.Y << "box z:: " << volumes[0].box.min.Z << std::endl;
+}
---
+++ GIT/src/simulation.h
@@ -0,0 +1,250 @@
+#pragma once
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+
+// Math.h - STD math Library
+#include <math.h>
+
+#include <vector>
+
+#include <string>
+
+#include <iostream>
+
+struct Vector2
+{
+	// Default Constructor
+	Vector2()
+	{
+		X = 0.0f;
+		Y = 0.0f;
+	}
+	// Variable Set Constructor
+	Vector2(double X_, double Y_)
+	{
+		X = X_;
+		Y = Y_;
+	}
+	// Bool Equals Operator Overload
+	bool operator==(const Vector2& other) const
+	{
+		return (this->X == other.X && this->Y == other.Y);
+	}
+	// Bool Not Equals Operator Overload
+	bool operator!=(const Vector2& other) const
+	{
+		return !(this->X == other.X && this->Y == other.Y);
+	}
+	// Addition Operator Overload
+	Vector2 operator+(const Vector2& right) const
+	{
+		return Vector2(this->X + right.X, this->Y + right.Y);
+	}
+	// Subtraction Operator Overload
+	Vector2 operator-(const Vector2& right) const
+	{
+		return Vector2(this->X - right.X, this->Y - right.Y);
+	}
+	// double Multiplication Operator Overload
+	Vector2 operator*(const double& other) const
+	{
+		return Vector2(this->X *other, this->Y * other);
+	}
+
+	// Scalar Product Operator Overload
+	Vector2 operator*(const Vector2& right) const
+	{
+		return Vector2(this->X * right.X, this->Y * right.Y);
+	}
+	// Vectorial Product Operator Overload
+	double operator%(const Vector2& right) const
+	{
+		return double(this->X*right.Y - this->Y*right.X);
+	}
+
+	// Positional Variables
+	double X;
+	double Y;
+};
+
+struct Vector3
+{
+	// Default Constructor
+	Vector3()
+	{
+		X = 0.0f;
+		Y = 0.0f;
+		Z = 0.0f;
+	}
+	// Variable Set Constructor
+	Vector3(double X_, double Y_, double Z_)
+	{
+		X = X_;
+		Y = Y_;
+		Z = Z_;
+	}
+	// Bool Equals Operator Overload
+	bool operator==(const Vector3& other) const
+	{
+		return (this->X == other.X && this->Y == other.Y && this->Z == other.Z);
+	}
+	// Bool Not Equals Operator Overload
+	bool operator!=(const Vector3& other) const
+	{
+		return !(this->X == other.X && this->Y == other.Y && this->Z == other.Z);
+	}
+	// Addition Operator Overload
+	Vector3 operator+(const Vector3& right) const
+	{
+		return Vector3(this->X + right.X, this->Y + right.Y, this->Z + right.Z);
+	}
+	// Subtraction Operator Overload
+	Vector3 operator-(const Vector3& right) const
+	{
+		return Vector3(this->X - right.X, this->Y - right.Y, this->Z - right.Z);
+	}
+	// double Multiplication Operator Overload
+	Vector3 operator*(const double& other) const
+	{
+		return Vector3(this->X * other, this->Y * other, this->Z * other);
+	}
+	// double Division Operator Overload
+	Vector3 operator/(const double& other) const
+	{
+		return Vector3(this->X / other, this->Y / other, this->Z / other);
+	}
+	
+	// Scalar Product Operator Overload
+	Vector3 operator*(const Vector3& right) const
+	{
+		return Vector3(this->X * right.X, this->Y * right.Y, this->Z * right.Z);
+	}
+	// Vectorial Product Operator Overload
+	Vector3 operator%(const Vector3& right) const
+	{
+		return Vector3(this->Y*right.Z - this->Z*right.Y, this->Z*right.X - this->X*right.Z, this->X*right.Y - this->Y*right.X);
+	}
+
+	// Positional Variables
+	double X;
+	double Y;
+	double Z;
+};
+
+struct Triangle3 {
+	Vector3 v1, v2, v3;
+};
+
+struct Triangle2 {
+	Vector2 v1, v2, v3;
+};
+
+struct Box {
+	Vector3 min;
+	Vector3 max;
+};
+
+struct Vertex {
+	// Position Vector
+	Vector3 Position;
+
+	// Normal Vector
+	Vector3 Normal;
+};
+
+struct Plane {
+	double a, b, c, d;// ax+by+cz+d=0
+};
+
+struct Mesh {
+	// Mesh Name
+	std::string MeshName;
+	// Vertex List
+	std::vector<Vertex> Vertices;
+	// Index List
+	std::vector<unsigned int> Indices;
+
+	//
+	std::vector<Plane> plane;
+};
+
+struct Volume {
+	Box box = { {0, 0, 0}, {0, 0, 0} };
+	bool containsGas = true;
+	bool containsSB = false;
+	bool isActive = true;
+	double deltaPotential = 0;
+	Mesh mesh;
+	/*it would e grate if we could assign a variable potential in the form
+	a*x + b*y + c*z + d, but it would add more unnecessary complexity to the program and it will slow down the simulation a lot,
+	maybe I'll do this in a future version
+	*/
+};
+
+struct GasParticle {
+	double potential = 0;
+	Vector3 position;
+	Vector3 speed;
+};
+
+
+
+struct SimulationSttings {
+    double dt = 0.01;// max step dt
+	double gasRadius = 0.001;// particles will collide when distance = 2 * gasRadius
+    double finalTime = -1;// simulation end time, -1 to disable
+    int maxStepNumber = 10;// maximum number of simulation steps
+    double gasTemperature = 1;// average of points' kinetic energy
+    int gasPointsNumber = 10;
+    // TODO expand if necessary...
+};
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------
+
+class Simulation { // TODO reorganize in alphabetic order
+public:
+	//Simulation(float gasParticleNumber, double temperature);// TODO to complete
+
+	bool addVolume(Volume);
+	void ciao1(void) const { std::cout << "\nCiao dalla SIMULAZIONE!!!\n"; }// 1!!!!!!!!!!!!!!!!!!!!!!!1
+	int getVolumesNumber(void) const;
+	void setGasParticleNumber(int);
+	int getGasParticleNumber(void) const;
+	void setInitialTemperature(double);
+	double getInitialTemperature(void) const;
+	bool createGasParticles(void);
+	bool performOneStep(void);
+
+	//TODO delete these functions...
+	void printVertex(int) const;
+	void printPlanes(int) const;
+	void printPoints(void) const;
+	void test(void) const;
+	//bool loadObjData(std::string);// load obj file
+    //bool loadContainer(std::string);
+
+private:
+	bool isInsideVolume(Vector3, int) const;//const;// (point, volume index)
+	bool isInside2dTriangle(Triangle2, Vector2) const;
+	Box boundingBox(Volume) const;
+	Box boundingBox(int) const;
+	std::vector<int> volumesContainingVector(Vector3) const;
+	double potential(Vector3) const;
+	bool isInAllowedVolume(Vector3) const;
+	
+	double temperature = 1;
+	double GasParticleNumber = 10;
+	std::vector<Volume> volumes;
+	std::vector<GasParticle> gasParticles;
+    
+};
+
+
+#endif // SIMULATION_H
