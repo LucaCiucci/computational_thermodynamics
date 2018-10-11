3403f565b4261989c4cf6287ac7c5305e2f87b9a
Luca Ciucci <43919066+LucaCiucci@users.noreply.github.com>
2018-10-11 14:39:45 +0200

    Add files via upload

---
+++ src/mathFunctions.h
@@ -0,0 +1,17 @@
+#pragma once
+
+constexpr double Epsilon = 0.000000001;
+
+int sign(double x);/* {
+	if (x == 0) return 0;
+	if (x > 0) return 1;
+	return -1;
+}*/
+
+double fRand(double fMin, double fMax);/*
+{
+	
+	double f = (double)rand() / RAND_MAX;
+	return fMin + f * (fMax - fMin);
+	
+}*/
